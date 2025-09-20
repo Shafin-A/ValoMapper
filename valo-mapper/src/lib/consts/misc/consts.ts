e1f90ef575eefa47214ecd116df38184ff929538
Shafin Ahmed <kazishafin@gmail.com>
2025-09-20 13:58:48 -0400

    Move minimaps and add listviewicons folder

--- valo-mapper/src/lib/consts/misc/consts.ts
+++ valo-mapper/src/lib/consts/misc/consts.ts
@@ -1,6 +1,5 @@
 export const TEMP_DRAG_ID = "temp-drag-id";
 
-export const ASCENT_MAP = "/maps/ascent.webp";
 export const SIDEBAR_WIDTH = "20rem";
 export const SCALE_FACTOR = 1.25;
 export const MAP_SIZE = 1000;
@@ -15,3 +14,93 @@ export const ROLE_ICONS: Record<string, string> = {
 };
 
 export const PIXELS_PER_METER = 122 / 15;
+
+export const MINIMAPS = {
+  Abyss: "/maps/minimaps/abyss.webp",
+  Ascent: "/maps/minimaps/ascent.webp",
+  Bind: "/maps/minimaps/bind.webp",
+  Breeze: "/maps/minimaps/breeze.webp",
+  Corrode: "/maps/minimaps/corrode.webp",
+  Fracture: "/maps/minimaps/fracture.webp",
+  Haven: "/maps/minimaps/haven.webp",
+  Icebox: "/maps/minimaps/icebox.webp",
+  Lotus: "/maps/minimaps/lotus.webp",
+  Pearl: "/maps/minimaps/pearl.webp",
+  Split: "/maps/minimaps/split.webp",
+  Sunset: "/maps/minimaps/sunset.webp",
+};
+
+export const MAP_OPTIONS = [
+  {
+    id: "abyss",
+    text: "Abyss",
+    src: "/maps/listviewicons/abyss.png",
+    textColor: "text-white",
+  },
+  {
+    id: "ascent",
+    text: "Ascent",
+    src: "/maps/listviewicons/ascent.png",
+    textColor: "text-white",
+  },
+  {
+    id: "bind",
+    text: "Bind",
+    src: "/maps/listviewicons/bind.png",
+    textColor: "text-white",
+  },
+  {
+    id: "breeze",
+    text: "Breeze",
+    src: "/maps/listviewicons/breeze.png",
+    textColor: "text-white",
+  },
+  {
+    id: "corrode",
+    text: "Corrode",
+    src: "/maps/listviewicons/corrode.png",
+    textColor: "text-white",
+  },
+  {
+    id: "fracture",
+    text: "Fracture",
+    src: "/maps/listviewicons/fracture.png",
+    textColor: "text-white",
+  },
+  {
+    id: "haven",
+    text: "Haven",
+    src: "/maps/listviewicons/haven.png",
+    textColor: "text-white",
+  },
+  {
+    id: "icebox",
+    text: "Icebox",
+    src: "/maps/listviewicons/icebox.png",
+    textColor: "text-white",
+  },
+  {
+    id: "lotus",
+    text: "Lotus",
+    src: "/maps/listviewicons/lotus.png",
+    textColor: "text-white",
+  },
+  {
+    id: "pearl",
+    text: "Pearl",
+    src: "/maps/listviewicons/pearl.png",
+    textColor: "text-white",
+  },
+  {
+    id: "split",
+    text: "Split",
+    src: "/maps/listviewicons/split.png",
+    textColor: "text-white",
+  },
+  {
+    id: "sunset",
+    text: "Sunset",
+    src: "/maps/listviewicons/sunset.png",
+    textColor: "text-white",
+  },
+];
