03c1bc5abb1870a148c533d29ed9d4a709f5f0f8
Shafin Ahmed <kazishafin@gmail.com>
2025-12-30 19:17:30 -0500

    Fix lobby page check

--- valo-mapper/src/components/providers/tour-provider.tsx
+++ valo-mapper/src/components/providers/tour-provider.tsx
@@ -13,7 +13,8 @@ import { tourSteps } from "@/components/tour/tour-steps";
 const TourContent = ({ children }: { children: React.ReactNode }) => {
   const { hasCompletedTour, startTour, isTourOpen } = useTour();
   const pathname = usePathname();
-  const isLobbyPage = pathname && pathname.match(/^\/[^\/]+$/);
+  const LOBBY_ID_REGEX = /^\/[A-Z2-7]{7,8}$/;
+  const isLobbyPage = pathname ? LOBBY_ID_REGEX.test(pathname) : false;
 
   useEffect(() => {
     if (!hasCompletedTour && !isTourOpen && isLobbyPage) {
