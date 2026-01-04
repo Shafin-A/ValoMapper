6753eedee579a8bd5ab4836b0628b73ac444cdc7
Shafin Ahmed <kazishafin@gmail.com>
2026-01-03 19:37:38 -0500

    Fix go linter

--- valo-mapper-api/routes/health.go
+++ valo-mapper-api/routes/health.go
@@ -9,6 +9,6 @@ import (
 func RegisterHealthRoutes(r *mux.Router) {
 	r.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("OK"))
+		_, _ = w.Write([]byte("OK"))
 	}).Methods("GET")
 }
